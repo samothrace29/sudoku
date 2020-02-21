b3c300de44e3571af92bdc81dd616de7c9dd7684
samothrace29 <34971701+samothrace29@users.noreply.github.com>
2020-02-21 06:58:33 +0100

    montre possibilités
    Montre dans le log les possibiliés restantes pour remplir la case.

--- script/sudoku.js
+++ script/sudoku.js
@@ -7,8 +7,8 @@
     * fill an array 2 dimension : 9*9 with all possible value
     */ 
 
-   function createTable (){
-    const tab = document.getElementById("tableSudoku");
+    function createTable () {
+        const tab = document.getElementById("tableSudoku");
         for (j=0;j<9;j++) {
             // Create line
             const column = document.createElement("TR");
@@ -54,7 +54,7 @@
             // remettre le focus sur la nouvelle case cliqué ( CSS )
             allSudokuElement[currentElement["row"]*9+currentElement["col"]].setAttribute ("actif","");
 
-            getPossibleValue ();
+           // getPossibleValue ();
         });
   });
 
@@ -115,13 +115,99 @@
    /* catch Event on button click and not on form submit */
    buttonSolveIt.addEventListener ('click', function  (event){
        event.preventDefault();
-       console.log ('Done ...');
+       console.log ('Resoudre le table, a ajouter');
+       let ligne  = getLigneValue();
+       let column = getColumnValue();
+       let square = getSquareValue();
+       let possibleValeur = Array (1,2,3,4,5,6,7,8,9);
+       console.log(" Nombre restant . ");
+       console.log(possibleValeur);
+       
+       
+
    });
 
 
    /**
-    * 
+    * retourne un table de nombre contenu dans la ligne courrante
     */
-   function getPossibleValue () {
-
+   function getLigneValue () {
+       let ligne = new Array();
+       for (j=0;j<9;j++) {
+            if ( allSudokuElement[currentElement["row"]*9+j].textContent.length > 0 )
+                ligne.push (parseInt ( allSudokuElement[currentElement["row"]*9+j].textContent ) );
+            else 
+                ligne.push ( null );
+    }
+    console.log(" ligne : ");   
+    console.log(ligne);
+    
+    return ligne;
    }
+
+   /**
+    * retourne un table de nombre contenu dans la column courrante
+    */
+   function getColumnValue () {
+       let column = new Array();
+       for (j=0;j<9;j++) {
+            if ( allSudokuElement[j*9+currentElement["col"]].textContent.length > 0 )
+                column.push ( parseInt ( allSudokuElement[j*9+currentElement["col"]].textContent ) );
+            else 
+                column.push ( null );
+    }
+    console.log(" column : ");   
+    console.log(column);
+    
+    return column;
+   }
+   /**
+    * retourne un table de nombre contenu dans le carré courrant
+    */
+   function getSquareValue () {
+       let squareArr = new Array();
+
+
+        let debutCol = parseInt ( currentElement["col"] /3 ) * 3;
+        let debutRow = parseInt ( currentElement["row"] /3 ) * 3;
+    
+        for (i=debutRow;i<debutRow+3;i++) {
+            for (j=debutCol;j<debutCol+3;j++) {
+                if ( allSudokuElement[i*9+j].textContent.length > 0 )
+                squareArr.push ( parseInt ( allSudokuElement[i*9+j].textContent ) );
+            else 
+                squareArr.push ( null );
+            }
+        }
+    console.log(" Square : ");
+    squareArr.splice (1,1);
+    console.log(squareArr.sort());
+    
+    return squareArr;
+   }
+
+   /**
+    * retourne un tableau des possibilité restante en fonction du tableau rentré
+    * ArrayComparaison : Array des possibilité restante
+    * arrayValueToRemove : Array de nombre qui seront retiré du arrayComparaison
+    * returne le arraComparaison sans les valeur dans arrayValueToRemove
+    */
+   function getAvailableValue ( arrayComparaison , arrayValueToRemove ) {
+    
+    for ( i=0 ; i<arrayValueToRemove.length ; i++ ) {
+        
+        if ( arrayValueToRemove[i] == null ) continue;
+
+        let found = arrayComparaison.indexOf (arrayValueToRemove[i]);
+
+        // si le nombre existe dans le table, l'enlever 
+        if (found != -1) {
+            arrayComparaison.splice (found,1);
+        }
+    }
+
+    // renvoyer le nouveau table sans les valeur qu on vient de retirer.
+    return arrayComparaison;
+   }
+
+
